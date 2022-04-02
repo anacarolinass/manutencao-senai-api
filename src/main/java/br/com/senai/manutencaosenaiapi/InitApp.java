b510af7e7b7b0d0b228530d0ebe3cb459e6201e0
Ana Carolina Santos <anacarolina.s.s.hotmail.com>
2022-04-01 22:13:10 -0300

    Criado os pacotes, configurado lombok

--- src/main/java/br/com/senai/manutencaosenaiapi/InitApp.java
+++ src/main/java/br/com/senai/manutencaosenaiapi/InitApp.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
+import br.com.senai.manutencaosenaiapi.entity.Tecnico;
+
 @SpringBootApplication
 public class InitApp {
 
@@ -19,6 +21,18 @@ public class InitApp {
 		return args -> {
 		
 			System.out.println("Deu certo");
+			
+			Tecnico tecnico = new Tecnico ();
+			tecnico.setId(1);
+			
+			Tecnico outroTecnico = new Tecnico();
+			outroTecnico.setId(1);
+			
+			System.out.println(tecnico);
+			System.out.println(outroTecnico);
+			if (tecnico.equals(outroTecnico)) {
+				System.out.println("São os mesmos");
+			}
 	};
 	
 	}
