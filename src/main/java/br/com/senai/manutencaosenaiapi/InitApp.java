ee62fc316eaccad46c6c10e8cd5a29fa6e2ad5de
Ana Carolina Santos <anacarolina.s.s.hotmail.com>
2022-04-08 22:09:54 -0300

    Criada a camada service para peça. Realizada refatoração das demais camadas services

--- src/main/java/br/com/senai/manutencaosenaiapi/InitApp.java
+++ src/main/java/br/com/senai/manutencaosenaiapi/InitApp.java
@@ -2,6 +2,8 @@ package br.com.senai.manutencaosenaiapi;
 
 import java.time.LocalDate;
 
+import javax.swing.plaf.synth.SynthScrollPaneUI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -9,7 +11,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
+import br.com.senai.manutencaosenaiapi.entity.Cliente;
+import br.com.senai.manutencaosenaiapi.entity.Peca;
 import br.com.senai.manutencaosenaiapi.entity.Tecnico;
+import br.com.senai.manutencaosenaiapi.enums.Sexo;
+import br.com.senai.manutencaosenaiapi.service.ClienteService;
+import br.com.senai.manutencaosenaiapi.service.PecaService;
 import br.com.senai.manutencaosenaiapi.service.TecnicoService;
 
 @SpringBootApplication
@@ -21,6 +28,12 @@ public class InitApp {
 	}
 	@Autowired
 	private TecnicoService service;
+	
+	@Autowired
+	private ClienteService clienteService;
+	
+	@Autowired
+	private PecaService pecaService;
 	
 	@Bean
 	public CommandLineRunner commandLineRunner (ApplicationContext ac) {
@@ -34,26 +47,39 @@ public class InitApp {
 				novoTecnico.setDataDeAdmissao(dataDeAdmissao);
 				this.service.inserir(novoTecnico); */
 				
-				Tecnico tecnicoSalvo = new Tecnico();
+				/*Tecnico tecnicoSalvo = new Tecnico();
 				tecnicoSalvo.setId(1);
 				tecnicoSalvo.setNomeCompleto("Joanecleidson");
 				tecnicoSalvo.setDataDeAdmissao(LocalDate.now());
 				
 				this.service.alterar(tecnicoSalvo);
 				
-				System.out.println("Técnico salvo com sucesso");
+				System.out.println("Técnico salvo com sucesso");*/
 				
+				/*this.service.listarPor("José");
+				this.service.removerPor(0);*/
+				
+				/*Cliente novoCliente = new Cliente();
+				novoCliente.setNome("Joao");
+				novoCliente.setSobrenome("Da Silva");
+				novoCliente.setCpf("338.138.290-14");
+				novoCliente.setSexo(Sexo.MASCULINO);
+				novoCliente.setEndereco("Rua das couves");
+				novoCliente.setDataDeNascimento(LocalDate.of(2018, 9, 14));
+				this.clienteService.inserir(novoCliente);
+				System.out.println("Cliente salvo com sucesso");*/
+				Peca novaPeca = new Peca();
+				novaPeca.setDescricao("Teclado");
+				novaPeca.setQtdEmEstoque(10);
+				this.pecaService.inserir(null);
+				System.out.println(novaPeca);
 				
 			} catch (Exception e) {
 				
 				System.out.println(e.getMessage());
 				
 			}
-			
-		
-		
-				
-			
+
 	};
 	
 	}
--- src/main/java/br/com/senai/manutencaosenaiapi/service/TecnicoService.java
+++ src/main/java/br/com/senai/manutencaosenaiapi/service/TecnicoService.java
@@ -1,6 +1,12 @@
 package br.com.senai.manutencaosenaiapi.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 import org.springframework.stereotype.Service;
@@ -10,30 +16,46 @@ import com.google.common.base.Preconditions;
 
 import br.com.senai.manutencaosenaiapi.entity.Tecnico;
 
-@Service 
+@Service
 @Validated
 public class TecnicoService {
-	
-	public Tecnico inserir(@Valid 
-			@NotNull (message = "O técnico não pode ser nulo")
-			Tecnico novoTecnico) {
-		
-		Preconditions.checkArgument(novoTecnico.isNovo(), 
-				"O técnico já foi salvo");
-		
+
+	public Tecnico inserir(@Valid @NotNull(message = "O técnico não pode ser nulo") 
+		Tecnico novoTecnico) {
+
+		Preconditions.checkArgument(novoTecnico.isNovo(), "O técnico já foi salvo");
+
 		Tecnico tecnicoSalvo = novoTecnico;
-		
+
 		return tecnicoSalvo;
 	}
-	
+
 	public Tecnico alterar(@Valid 
-			@NotNull (message = "O tecnico não pode ser nulo")
-			Tecnico tecnicoSalvo) {
-		
+			@NotNull(message = "O tecnico não pode ser nulo") 
+		Tecnico tecnicoSalvo) {
+
 		Preconditions.checkArgument(!tecnicoSalvo.isNovo(), "O tecnico ainda não foi inserido");
-		
+
 		Tecnico tecnicoAtualizado = tecnicoSalvo;
 		return tecnicoAtualizado;
 
-}
+	}
+
+	public List<Tecnico> listarPor(
+			@NotEmpty(message = "O nome para a busca não pode ser nulo") 
+			@NotBlank(message = "Não pode haver espaços antes do nome")
+			String nome) {
+
+		return new ArrayList<Tecnico>();
+
+	}
+
+	public void removerPor(
+			@NotNull(message = "O id de exclusão não pode ser nulo") 
+			@Min(value = 1, message = "O id deve ser maior que zero") 
+			Integer id) {
+		// TODO implementar lógica de remoção
+
+	}
+
 }
