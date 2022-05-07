e398d51d85f5d62be10d31efe422ce420d4421f9
Ana Carolina Santos <anacarolina.s.s.hotmail.com>
2022-05-06 21:44:57 -0300

    Finalizado o repositorio de ordem de serviço

--- src/main/java/br/com/senai/manutencaosenaiapi/InitApp.java
+++ src/main/java/br/com/senai/manutencaosenaiapi/InitApp.java
@@ -1,11 +1,13 @@
 package br.com.senai.manutencaosenaiapi;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import javax.transaction.Transactional;
 
+import org.hibernate.mapping.Array;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -14,6 +16,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 import br.com.senai.manutencaosenaiapi.entity.Cliente;
+import br.com.senai.manutencaosenaiapi.entity.OrdemDeServico;
 import br.com.senai.manutencaosenaiapi.entity.Peca;
 import br.com.senai.manutencaosenaiapi.entity.Tecnico;
 import br.com.senai.manutencaosenaiapi.enums.Sexo;
@@ -29,38 +32,65 @@ public class InitApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InitApp.class, args);
-		
+
 	}
-	
+
 	@Autowired
 	private TecnicoService tecnicoService;
-	
+
 	@Autowired
 	private ClienteService clienteService;
-	
+
 	@Autowired
 	private PecaService pecaService;
-	
+
 	@Autowired
-	private OrdemDeServicoService ordemService;	
-	
-	@Bean	
+	private OrdemDeServicoService ordemService;
+
+	@Bean
 	public CommandLineRunner commandLineRunner(ApplicationContext ac) {
 		return args -> {
 			try {
-				Cliente novoCliente = new Cliente();
-				novoCliente.setNome("Jhonny");
-				novoCliente.setSobrenome("Depp");
-				novoCliente.setDataDeNascimento(LocalDate.of(1973, 3, 15));
-				novoCliente.setCpf("005.900.289-10");
-				novoCliente.setSexo(Sexo.M);
-				novoCliente.setEndereco("Rua josé das couves");
-				this.clienteService.inserir(novoCliente);
-			}catch (Exception e) {				
+				/*
+				 * List<Cliente> clientes = clienteService.listarPor("j"); Cliente
+				 * clienteSelecionado = clientes.get(0);
+				 * 
+				 * List<Tecnico> tecnicos = tecnicoService.listarPor("b"); Tecnico
+				 * tecnicoSelecionado = tecnicos.get(0);
+				 * 
+				 * List<Peca> pecas = pecaService.listarPor("p"); List<Peca> pecasDoReparo = new
+				 * ArrayList<>(); pecasDoReparo.add(pecas.get(0));
+				 * pecasDoReparo.add(pecas.get(1));
+				 * 
+				 * OrdemDeServico novaOrdem = new OrdemDeServico();
+				 * novaOrdem.setCliente(clienteSelecionado);
+				 * novaOrdem.setTecnico(tecnicoSelecionado);
+				 * novaOrdem.setDataDeAbertura(LocalDate.of(2022, 5, 6));
+				 * novaOrdem.setDescricaoDoProblema("Micro não liga");
+				 * novaOrdem.setPecasDoReparo(pecasDoReparo);
+				 */
+
+				/*
+				 * List<Cliente> clientes = clienteService.listarPor("j"); OrdemDeServico
+				 * ordemSalva = ordemService.buscarPor(6);
+				 * ordemSalva.setCliente(clientes.get(2)); ordemService.alterar(ordemSalva);
+				 * System.out.println(ordemSalva);
+				 */
+
+				OrdemDeServico ordemSalva = ordemService.buscarPor(6);
+				ordemSalva.getPecasDoReparo().add(ordemSalva.getPecasDoReparo().get(0));
+				ordemSalva.setDescricaoDoReparo("Poeira");
+				ordemSalva.setDataDeEncerramento(LocalDate.of(2022, 5, 6));
+				this.ordemService.fechar(ordemSalva);
+				
+				ordemService.fechar(ordemSalva);
+				
+
+			} catch (Exception e) {
 				System.out.println(e.getMessage());
 			}
-			
+
 		};
-	}	
+	}
 
 }
